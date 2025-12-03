cbfc421b3e4fa5f4a9889350b792513ef9ab4057
Wearily <parsapournabi1382@gmail.com>
2025-12-03 21:59:11 +0330

    init SignalSlot files

---
+++ SignalSlot/main.cpp
@@ -0,0 +1,25 @@
+#include <QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QQmlContext>
+
+int main(int argc, char* argv[])
+{
+    qputenv("QT_ASSUME_STDERR_HAS_CONSOLE", "1");
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+
+    engine.load(url);
+    return app.exec();
+
+}
---
+++ SignalSlot/main.qml
@@ -0,0 +1,11 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+import QtQuick.Layouts 1.12
+
+Window {
+    width: 800
+    height: 620
+    title: qsTr("SignalSlot")
+    visible: true
+
+}
